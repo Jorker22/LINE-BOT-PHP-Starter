8837c79bd8098ef2337f7d8b29634d190f20ba8b
Jorker22 <chaiyut5517@gmail.com>
2017-10-26 20:13:00 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'Do') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message'] == 'Do') {
 			// Get text sent
 			$text = 'Hmm.';
 			// Get replyToken
