d41aa7d7c1c6889c6fd39cb8c64eda5188a1b319
Jorker22 <chaiyut5517@gmail.com>
2017-10-26 19:50:11 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'egQa5x0UdLUqsow7a/d53vRdVsB0q48+tYde+a46llCx6qY4dm2LMs3AfY5zIOXiqJaFpezfa79yM2DqWOQ86D1sUoDBLMjsqDPsbXHiumQYAYiM6lVlM/oiUNaYHuSZWUdPH0iqywbYQOncNkHG+gdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
